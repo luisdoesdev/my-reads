4fa10dd41f2875593d3d101272d03267c0191c90
letorruella <letorruella@gmail.com>
2017-12-10 04:06:59 -0500

    Modulate Search Screen

--- src/BooksRead.js
+++ src/BooksRead.js
@@ -6,12 +6,16 @@ class BooksRead extends Component{
 
 render(){
     const {read} = this.props
+    console.log(read)
     return(
                   <div className="bookshelf">
                   <h2 className="bookshelf-title">Currently Reading</h2>
                   <div className="bookshelf-books">
                     <ol className="books-grid">
-                      <li>
+                    {read.map((r)=>{
+
+                    
+                      <li key= {r.shelf}>
                         <div className="book">
                           <div className="book-top">
                             <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: 'url("http://books.google.com/books/content?id=PGR2AwAAQBAJ&printsec=frontcover&img=1&zoom=1&imgtk=AFLRE73-GnPVEyb7MOCxDzOYF1PTQRuf6nCss9LMNOSWBpxBrz8Pm2_mFtWMMg_Y1dx92HT7cUoQBeSWjs3oEztBVhUeDFQX6-tWlWz1-feexS0mlJPjotcwFqAg6hBYDXuK_bkyHD-y&source=gbs_api")' }}></div>
@@ -25,11 +29,12 @@ render(){
                               </select>
                             </div>
                           </div>
-                          <div className="book-title">To Kill a Mockingbird</div>
-                          <div className="book-authors">Harper Lee</div>
+                          <div className="book-title">{r.title}}</div>
+                          <div className="book-authors">{r.author}</div>
                         </div>
                       </li>
               
+                    })}
                     </ol>
                   </div>
                 </div>
