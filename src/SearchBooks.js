3f1ae97541045b6995678c57f7d971a1d0bea828
letorruella <letorruella@gmail.com>
2017-12-10 22:48:26 -0500

    added the search method

--- src/SearchBooks.js
+++ src/SearchBooks.js
@@ -1,15 +1,69 @@
 import React, { Component } from 'react'
+import * as BooksAPI from './BooksAPI'
 
 import { Link } from 'react-router-dom'
-
+import escapeRegExp from 'escape-string-regexp'
+import sortBY from 'sort-by'
 
 
 class SearchBooks extends Component{
 
+state={
+    books:[],
+    query: ''
+}
+
+  // call The API
+ componentDidMount(){
+     this.updateQuery()
+ }
+ 
+
+
+
+  updateQuery = ( query, books ) =>{
+      
+    this.setState({
+          query: query.trim()
+      })
+
+    BooksAPI.search(query).then((books)=>{
+        this.setState({books})
+    })     
+      
+  }
+
+
+
+  //Clear query
+  clearQuery = ( query ) =>{
+    this.setState({
+        query: query = " "
+    })
+}
 
 
 
 render(){
+    const {books, query} = this.state
+   
+    console.log(books)
+    
+    let filterBooks 
+
+    if ( query ){
+        const match = new RegExp(escapeRegExp(query), 'i')  
+        filterBooks = books.filter((book)=> match.test(book.title))
+
+    } 
+    else {
+        filterBooks = this.state.books
+    }
+
+
+
+      
+
     return(
        
              <div className="search-books">
@@ -22,12 +76,22 @@ render(){
 
               <div className="search-books-input-wrapper">
               
-                <input type="text" placeholder="Search by title or author"/>
+                <input 
+                type="text" 
+                placeholder="Search by title or author"
+                onChange = {(e) => this.updateQuery(e.target.value)}
+                />
 
               </div>
             </div>
             <div className="search-books-results">
-              <ol className="books-grid"></ol>
+              <ol className="books-grid">
+              
+              {filterBooks.map((b)=>(
+                  <li>{b.title}</li>
+              ))}
+              
+              </ol>
             </div>
           </div>
         
